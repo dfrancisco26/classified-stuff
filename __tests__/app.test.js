2fb6568e2cb46074152d3505c7b1680714b5e422
David Francisco <dfrancisco2677@gmail.com>
2022-08-15 08:56:21 -0700

    Create user route functions properly. Had issues trying to use first and last name, decided to take them out for simplicity.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,30 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const mockUser = {
+  email: 'dndfreak@gmail.com',
+  password: 'fourfour44'
+};
+
+describe('CRUD routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
+    
   });
 });
