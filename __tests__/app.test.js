376be9ea492c4e19ee79330fb0621fb68296194c
David Francisco <dfrancisco2677@gmail.com>
2022-08-19 16:09:57 -0700

    Created a protected route to get a message. Uses authenticate middleware that requires user to be signed in.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,11 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'dndfreak@gmail.com',
   password: 'fourfour44'
 };
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
 
 describe('CRUD routes', () => {
   beforeEach(() => {
@@ -29,6 +41,17 @@ describe('CRUD routes', () => {
       .send({ email: 'dndfreak@gmail.com', password: 'fourfour44' });
     expect(res.status).toEqual(200);
   });
+
+  it('/protected should return a 401 if not authenticated', async () => {
+    const res = await request(app).get('/api/v1/users/protected');
+    expect(res.status).toEqual(401);
+  });
+
+  // it('/protected should return the current user if authenticated', async () => {
+  //   const [agent] = await registerAndLogin();
+  //   const res = await agent.get('/api/v1/users/protected');
+  //   expect(res.status).toEqual(200);
+  // });
 
   afterAll(() => {
     pool.end();
